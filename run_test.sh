e8f923e7afce33fd6aeaee441939fa591951b574
Nicolas Pauss <nicolas.pauss@intersec.com>
2026-01-20 10:54:44 +0100

    Adapt test for `cargo bisect-rustc`.
    I was not able to detect since when it failed. All tested versions
    failed.
    I tried up to '2023-03-07'. Older versions hangs on compilation on my
    computer.
    
    `cargo bisect-rustc -v --end 2023-03-15 --script ./run_test.sh`

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,20 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
+
+set -o pipefail
+
+
+rust_toolchain="nightly"
+if [ -n "${RUSTUP_TOOLCHAIN:-}" ]; then
+    rust_toolchain="${RUSTUP_TOOLCHAIN//bisector-/}"
+    rustup component add rust-src --toolchain "$rust_toolchain" 2>&1 | \
+        tee -a script_output.txt
+fi
 
 RUSTFLAGS=-Zsanitizer=address \
     cargo \
-    +nightly \
+    "+$rust_toolchain" \
     test \
     -Zbuild-std=panic_abort,std \
     -Zpanic-abort-tests \
-    --target x86_64-unknown-linux-gnu
+    --target x86_64-unknown-linux-gnu 2>&1 | \
+        tee -a script_output.txt
